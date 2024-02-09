46eb79a55e7c7640fbe64fe4eb68dcd83b8d8171
Vitaliy Dechyshyn <demchyshyn.v87@gmail.com>
2024-02-09 17:53:03 +0100

    only one variant phone number

--- backend/core/enums/regex_enum.py
+++ backend/core/enums/regex_enum.py
@@ -12,8 +12,8 @@ class RegExEnum(Enum):
     )
 
     PHONE = (
-        r'^(38)?0\d{9}$',
-        'only numbers, example: 380xxxxxxxxx, 0xxxxxxxxx'
+        r'^380\d{9}$',
+        'only numbers, example: 0xxxxxxxxx'
 
     )
 
